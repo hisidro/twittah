3c1901347692e4e413b43bbf50fd321e4836d21c
Homer Isidro <homer.isidro@gmail.com>
2015-03-04 15:33:59 +0800

    First commit to initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittah::Application.config.secret_token = 'c7c76bc7dd19ba0418d1594e8ba0ffb76e0ab57da23febec69a272b2fb4bfb8fc469d9834fc6dc98fc2af02d100fe502bc95d607ff35a051bd06a19116966590'
